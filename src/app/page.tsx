7b1db402590a7b0cbc3b77e5f31129cdfaf3f0d6
moto <moto@moto-iMac27.local>
2024-09-23 11:42:21 +0900

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,153 @@
-import Image from "next/image";
+'use client'
 
-export default function Home() {
+import { useContext, useEffect, useMemo, useState } from 'react'
+import {
+  getFirestore,
+  collection,
+  CollectionReference,
+  query,
+  orderBy,
+  onSnapshot,
+  doc,
+  getDoc,
+  setDoc,
+  addDoc,
+  deleteDoc,
+} from 'firebase/firestore'
+import ChatContainer from '@/components/chat-container'
+import { FirebaseUserContext } from '@/lib/firebase-user'
+import { preparePrompt } from '@/lib/prepare-prompt'
+import { FirestoreMessageData, MessageData, prepareMessage } from '@/lib/message'
+import { State, STATES } from '@/lib/states'
+import { FirestoreUserData } from '@/lib/user-data'
+
+/**
+ * A portal page with an ai chat.
+ *
+ * Using <ChatContainer>` component.
+ *
+ * Using `messages` (state variable) subscribed to Firestore collection `messages`
+ * with `use` hook.
+ */
+const ChatPage = () => {
+  const user = useContext(FirebaseUserContext)
+  const uid = user.currentUser?.uid
+  const db = getFirestore()
+
+  const [messages, setMessages] = useState<MessageData[]>([])
+  const [userData, setUserData] = useState<FirestoreUserData>()
+
+  // ------------
+  // messages
+  // ------------
+
+  const messagesCollection = useMemo(
+    () => collection(db, `users/${uid}/messages`) as CollectionReference<FirestoreMessageData>,
+    [db, uid]
+  )
+
+  useEffect(() => {
+    // Geminiの responseが書き込まれた
+    const unsubscribe = onSnapshot(query(messagesCollection, orderBy('createTime', 'asc')), {}, (snapshot) => {
+      const messages = snapshot.docs.map((doc) => prepareMessage(doc.data(), doc.id))
+
+      console.log(
+        'Message doc changes: ',
+        snapshot.docChanges().map((ch) => ({ type: ch.type, id: ch.doc.id, doc: ch.doc.data() }))
+      )
+
+      setMessages(messages)
+    })
+
+    return unsubscribe
+  }, [messagesCollection, uid])
+
+  // user promptを書き込む
+  const sendMessage = async (userMsg: string) => {
+    const state = userData?.state
+
+    if (state) {
+      // user promptをすぐに表示する
+      setMessages((prev) => [...prev, { prompt: userMsg }])
+
+      // Geminiへ messageを送信する
+      const newMessageRef = await addDoc(messagesCollection, {
+        // 'prompt'へ書き込む
+        prompt: preparePrompt(state, userMsg, messages), // 事前プロンプトを埋め込む
+      })
+
+      console.log('New message added with ID: ', newMessageRef.id)
+    }
+  }
+
+  // messageを削除する
+  const deleteMessage = async (messageId: string) => {
+    await deleteDoc(doc(messagesCollection, messageId))
+  }
+
+  const deleteAllMessages = async () => {
+    for (const message of messages) {
+      const messageId = message.id
+
+      if (messageId) {
+        await deleteMessage(messageId)
+      }
+    }
+  }
+
+  // ------------
+  // user data
+  // ------------
+
+  // read
+  useEffect(() => {
+    const fetchData = async () => {
+      const docRef = doc(db, `users/${uid}`)
+      const docSnap = await getDoc(docRef)
+
+      setUserData(
+        docSnap.exists()
+          ? (docSnap.data() as FirestoreUserData)
+          : {
+              state: STATES[0],
+            }
+      )
+    }
+
+    fetchData()
+  }, [db, uid])
+
+  const setState = async (state: State) => {
+    const userData0 = { ...userData, state: state }
+
+    setUserData(userData0)
+
+    // write
+    const docRef = doc(db, `users/${uid}`)
+
+    await setDoc(docRef, userData0)
+
+    /* update
+    await updateDoc(docRef, {
+      state: state,
+    } as Partial<FirestoreUserData>)
+    */
+  }
+
+  if (!userData) {
+    return <span />
+  }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    <ChatContainer
+      messages={messages}
+      onMessageSubmit={sendMessage}
+      onMessageDelete={deleteMessage}
+      deleteAllMessages={deleteAllMessages}
+      state={userData.state}
+      setState={setState}
+    />
+  )
 }
+
+export default ChatPage
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,95 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
+import formsPlugin from '@tailwindcss/forms'
+import plugin from 'tailwindcss/plugin'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      fontFamily: {
+        natume: ['Natume', 'sans-serif'],
+      },
+      fontSize: {
+        // [font-size, line-height(*1.25)]
+        xs: ['0.375rem', '0.46875rem'],
+        '3sm': ['0.75rem', '0.9375rem'],
+        '2sm': ['0.85rem', '1.0625rem'],
+        sm: ['0.875rem', '1.09375rem'],
+        base: ['1rem', '1.25rem'],
+        lg: ['1.25rem', '1.5625rem'],
+        xl: ['1.5rem', '1.875rem'],
+        '2xl': ['2rem', '2.5rem'],
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
       },
     },
   },
-  plugins: [],
-};
-export default config;
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      const newUtilities = {
+        '.screen-bg': {
+          // 'background-image': "url('/bg.png')",
+          'background-position': 'center',
+          'background-size': 'contain',
+          'background-repeat': 'no-repeat',
+
+          position: 'relative',
+          width: '100%',
+          'padding-top': '60.5769230769231%',
+        },
+        '.screen-inside': {
+          position: 'absolute',
+          top: '0',
+          left: '0',
+          width: '100%',
+          height: '100%',
+          overflow: 'hidden',
+        },
+        '.screen-content': {
+          width: '100%',
+          height: '85.18%',
+          'overflow-y': 'auto', // 縦方向のスクロール: 有効
+        },
+
+        '.fukidashi-hito': {
+          'border-image-source': 'url("/fukidashi-hito.png")',
+          'border-image-slice': '56 56 56 56', // 上右下左 ※残りは余白
+          'border-width': '14px 14px 14px 14px',
+          'border-image-repeat': 'stretch',
+          'border-image-width': '1',
+        },
+        '.fukidashi-cat': {
+          'border-image-source': 'url("/fukidashi-cat.png")',
+          'border-image-slice': '56 56 56 114', // 上右下左 ※残りは余白
+          'border-width': '14px 14px 14px 28.5px',
+          'border-image-repeat': 'stretch',
+          'border-image-width': '1',
+        },
+        '.fukidashi-text-hito': {
+          color: '#111827',
+          'background-color': '#eee3cc',
+          'font-size': '24px',
+          'text-align': 'center',
+        },
+        '.fukidashi-text-cat': {
+          color: '#111827',
+          'background-color': '#fff',
+          'font-size': '24px',
+          'text-align': 'center',
+        },
+      }
+
+      addUtilities(newUtilities)
+    }),
+
+    formsPlugin,
+  ],
+}
+
+export default config
